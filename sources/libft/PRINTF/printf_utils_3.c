f6ff7cf68d4aeaa255755737d338e59483840b0c
NJ <nj@MacBook-Air-de-NJ.local>
2021-04-16 01:01:17 +0200

    included libft working, makefile to clean

--- sources/libft/PRINTF/printf_utils_3.c
+++ sources/libft/PRINTF/printf_utils_3.c
@@ -12,35 +12,6 @@
 
 #include "../includes/ft_printf.h"
 
-int			ft_atoi(const char *nptr)
-{
-	int			nb;
-	const char	*temp;
-	int			u;
-
-	u = 1;
-	nb = 0;
-	temp = nptr;
-	while ((*temp >= 9 && *temp <= 13) || *temp == ' ')
-		temp++;
-	if (*temp == '-' || *temp == '+')
-		temp++;
-	while (*temp >= '0' && *temp <= '9')
-		temp++;
-	while ((temp != nptr) || ((*temp < 9 && *temp > 13) && *temp != ' '))
-	{
-		temp--;
-		if (*temp >= '0' && *temp <= '9')
-		{
-			nb += (*temp - 48) * u;
-			u *= 10;
-		}
-		else if (*temp == '-')
-			nb *= -1;
-	}
-	return (nb);
-}
-
 int			max(int a, int b)
 {
 	if (a > b)
