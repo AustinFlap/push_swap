97bc48ccc88fd8c4e4512cf94885da6df5c1b65b
NJ <nj@MacBook-Air-de-NJ.local>
2021-10-04 14:32:22 +0200

    code complete

--- sources/sort_special.c
+++ sources/sort_special.c
@@ -6,11 +6,59 @@
 /*   By: avieira <avieira@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 12:47:55 by avieira           #+#    #+#             */
-/*   Updated: 2021/10/04 13:35:57 by avieira          ###   ########.fr       */
+/*   Updated: 2021/10/04 14:28:59 by avieira          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+void		get_find_a(int find[2], t_stacks *stacks)
+{
+	int		i;
+	int		min_delta[2];
+	int		delta;
+
+	min_delta[0] = - (*stacks->len_b + *stacks->len_a);
+	min_delta[1] = (*stacks->len_b + *stacks->len_a);
+	i = -1;
+	while (++i < *stacks->len_a)
+	{
+		delta = stacks->a[i] - *stacks->b;
+		if (delta < 0 && delta > min_delta[0])
+		{
+			find[0] = i + 1;
+			min_delta[0] = delta;
+		}
+		else if (delta > 0 && delta < min_delta[1])
+		{
+			find[1] = i;
+			min_delta[1] = delta;
+		}
+	}
+}
+
+void		rearange_a(t_stacks *stacks, t_input *input)
+{
+	void	(*rotate)(t_stacks *, char, t_input *);
+	int		find[2];
+	int		found;
+
+	find[0] = -1;
+	find[1] = -1;
+	get_find_a(find, stacks);
+	found = find[0];
+	if (found == -1)
+		found = find[1];
+	rotate = &rotate_a;
+	if (found > (*stacks->len_a) / 2)
+	{
+		found = *stacks->len_a - found;
+		rotate = &reverse_rotate_a;
+	}
+	while (found-- > 0)
+		rotate(stacks, 1, input);
+}
+
 
 void		sort_little(t_input *input, t_stacks *stacks)
 {
@@ -39,4 +87,11 @@ void		sort_little(t_input *input, t_stacks *stacks)
 		rotate_a(stacks, 1, input);
 	if (stacks->a[2] < stacks->a[0] && stacks->a[2] < stacks->a[1])
 		reverse_rotate_a(stacks, 1, input);
+	while (*stacks->len_b)
+	{
+		rearange_a(stacks, input);
+		push_a(stacks, 1, input);
+	}
+	while (*stacks->a)
+		rotate_a(stacks, 1, input);
 }
